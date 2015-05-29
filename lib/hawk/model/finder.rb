b1c127a36226acc7ab28c675edf9e455d7d159df
Marcello Barnaba <vjt@openssl.it>
2015-05-29 18:43:19 +0200

    Add .all()

--- lib/hawk/model/finder.rb
+++ lib/hawk/model/finder.rb
@@ -9,13 +9,24 @@ module Hawk
       module ClassMethods
         def find(id)
           repr = connection.get([model_path, id].join('/'))
-          repr = repr.fetch(model_key) if repr.key?(model_key)
+          repr = repr.fetch(instance_key) if repr.key?(instance_key)
 
           new repr
         end
 
-        def model_key
-          @_model_key ||= self.name.underscore
+        def all
+          repr = connection.get(model_path)
+          repr = repr.fetch(collection_key) if repr.key?(collection_key)
+
+          repr.map! {|instance| new instance}
+        end
+
+        def instance_key
+          @_instance_key ||= self.name.underscore
+        end
+
+        def collection_key
+          @_collection_key = instance_key.pluralize
         end
 
         def model_path(path = nil)
