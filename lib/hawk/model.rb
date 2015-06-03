a7b70450e04b46a1a2b0b97b834dadf6b0b386b3
Marcello Barnaba <vjt@openssl.it>
2015-06-04 00:50:45 +0200

    Whitespace

--- lib/hawk/model.rb
+++ lib/hawk/model.rb
@@ -1,12 +1,12 @@
 module Hawk
 
   module Model
-    autoload :Base,       'hawk/model/base'
-    autoload :Schema,     'hawk/model/schema'
-    autoload :Connection, 'hawk/model/connection'
-    autoload :Finder,     'hawk/model/finder'
-    autoload :Querying,   'hawk/model/querying'
-    autoload :Proxy,      'hawk/model/proxy'
+    autoload :Base,        'hawk/model/base'
+    autoload :Schema,      'hawk/model/schema'
+    autoload :Connection,  'hawk/model/connection'
+    autoload :Finder,      'hawk/model/finder'
+    autoload :Querying,    'hawk/model/querying'
+    autoload :Proxy,       'hawk/model/proxy'
     autoload :Association, 'hawk/model/association'
   end
 
