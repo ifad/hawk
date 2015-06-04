043472ca94f81e2b77736357b85277d265b1ee34
Marcello Barnaba <vjt@openssl.it>
2015-06-04 22:59:22 +0200

    Add after-schema blocks, useful to define aliases on generated attribute methods

--- lib/hawk/model/schema.rb
+++ lib/hawk/model/schema.rb
@@ -45,9 +45,8 @@ module Hawk
       module ClassMethods
         def inherited(subclass)
           super
-          if self.schema
-            subclass.instance_variable_set :@_schema, self.schema
-          end
+          subclass.instance_variable_set :@_schema,       self.schema       if self.schema
+          subclass.instance_variable_set :@_after_schema, self.after_schema if self.after_schema
         end
 
         def schema
@@ -62,6 +61,15 @@ module Hawk
 
             attr_reader key
           end
+
+          if after_schema
+            class_eval(&after_schema)
+          end
+        end
+
+        def after_schema(&block)
+          @_after_schema = block if block
+          @_after_schema
         end
       end
 
