e85b972638010fa36f4459daa0f4b52cf3d4aedf
Marcello Barnaba <vjt@openssl.it>
2015-06-23 14:35:23 +0200

    Reset limit & offset when fetching associations

--- lib/hawk/model/association.rb
+++ lib/hawk/model/association.rb
@@ -190,6 +190,8 @@ module Hawk
                 @_#{entities} ||= #{parent}::#{klass}.where(self.params.deep_merge(
                   #{key}: self.id,
                   from:   #{from.inspect},
+                  limit:  nil,
+                  offset: nil,
                 ))
               end
             RUBY
