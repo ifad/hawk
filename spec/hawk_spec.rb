1574a4906d510051ac4ae1548fa999ed4c3388bf
Paolo Zaccagnini <paolo@pzac.net>
2016-04-21 12:02:24 +0200

    Add equality spec

--- spec/hawk_spec.rb
+++ spec/hawk_spec.rb
@@ -7,12 +7,39 @@ describe Hawk do
 
   context 'given a class that inherits from Hawk::Model::Base' do
 
-    class Person < Hawk::Model::Base
+    class Owl < Hawk::Model::Base
+      schema do
+        integer :id
+      end
+    end
+
+    class Pig < Hawk::Model::Base
+      schema do
+        integer :id
+      end
     end
 
     it 'behaves like a Hawk model' do
-      object = Person.new
+      object = Owl.new
       expect(object).to be_kind_of(Hawk::Model::Base)
+    end
+
+    describe 'equality operator' do
+      it 'returns true if two objects have the same id and class' do
+        a = Owl.new; a.id = 123
+        b = Owl.new; b.id = 123
+
+        expect(a == b).to be_truthy
+      end
+
+      it 'fails otherwise' do
+        a = Owl.new; a.id = 123
+        b = Owl.new; b.id = 124
+        c = Pig.new; c.id = 123
+
+        expect(a == b).to be_falsy
+        expect(a == c).to be_falsy
+      end
     end
   end
 
