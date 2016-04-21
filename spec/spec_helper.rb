c26d84c11dc50d8bf613a887067e37fc8dd7ffbb
Paolo Zaccagnini <paolo@pzac.net>
2016-04-21 10:44:56 +0200

    Add a Dalli::Client mock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,15 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'hawk'
 require 'pry'
 require 'webmock/rspec'
+require 'support/dalli_client_mock'
 
 WebMock.disable_net_connect!
 
 Hawk::HTTP::Instrumentation.suppress_verbose_output true
+
+RSpec.configure do |config|
+  config.before do
+    allow(Dalli::Client).to receive(:new).and_return DalliClientMock.new
+  end
+end
+
---
+++ spec/support/dalli_client_mock.rb
@@ -0,0 +1,14 @@
+class DalliClientMock
+  def version
+    @version ||= Version.new
+  end
+
+  def get(_key); end
+  def set(_key, _value, _opts); end
+  def delete(_key); end
+
+  class Version
+    def fetch(_, _); 1.23; end
+  end
+
+end
